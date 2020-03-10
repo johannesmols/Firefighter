ae039383f11ff202fb141ea84ec5be8ab36b5399
Johannes Mols <johannes.mols@kraftvaerk.com>
2020-03-10 15:53:38 +0100

    Added custom tile types

---
+++ Assets/Scripts/Tiles/RoadTile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts.Tiles
+{
+    class RoadTile : Tile
+    {
+        [SerializeField]
+        private Sprite[] sprites;
+
+#if UNITY_EDITOR
+        [MenuItem("Assets/Create/Tiles/RoadTile")]
+        public static void CreateRoadTile()
+        {
+            var path = EditorUtility.SaveFilePanelInProject("Save Road Tile", "New Road Tile", "asset",
+                "Save road tile", "Assets");
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<RoadTile>(), path);
+            }
+        }
+#endif
+    }
+}
