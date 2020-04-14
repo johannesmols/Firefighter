4c3aae8857a0ebba3ebfd9ba66e0d05370ca821d
Johannes Mols <johannes.mols@googlemail.com>
2020-04-14 13:21:51 +0200

    added water tile

---
+++ Assets/Scripts/Tiles/WaterTile.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace Assets.Scripts.Tiles
+{
+    class WaterTile : AbstractGameTile
+    {
+        public WaterTile()
+        {
+            TileProperties = new TileProperties
+            {
+                IsMovable = false,
+                MovementCost = int.MaxValue,
+                IsFlammable = false,
+                IsGoal = false
+            };
+        }
+
+#if UNITY_EDITOR
+        [MenuItem("Assets/Create/Tiles/Water Tiles")]
+        public static void CreateWaterTile()
+        {
+            var path = EditorUtility.SaveFilePanelInProject("Save Water Tile", "New Water Tile", "asset",
+                "Save water tile", "Assets");
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                AssetDatabase.CreateAsset(CreateInstance<WaterTile>(), path);
+            }
+        }
+#endif
+    }
+}
